809e982c558dbe7dff5e2257f919058b69701341
Md Ali <66300613+xXxSpicyBoiiixXx@users.noreply.github.com>
2020-09-19 15:40:43 -0500

    Updating Lab 3 for Desktop

---
+++ Lab03/IncDate.java
@@ -0,0 +1,39 @@
+/*
+# FILE: IncDate.java
+# USAGE: --
+# DESCRIPTION: 
+# OPTIONS: --
+# REQUIREMENTS: User Input
+# BUGS: --
+# AUTHOR: xXxSpicyBoiiixXx (Md Ali)
+# ORGANIZATION: --
+# VERSION: 1.0
+# CREATED: 09/13/2020
+REVISION: --
+*/
+
+package Lab03;
+
+public class IncDate extends Date
+{
+
+  public IncDate(int newMonth, int newDay, int newYear)
+  {
+    super(newMonth, newDay, newYear);
+  }
+
+  // Increments this IncDate to represent the next day.
+  public void increment(){
+    /* 5. Filling your code here */
+
+    /* 5. End of code */
+  }
+
+  // reduce this IncDate to represent the previous day.
+  public void reduction(){
+    /* 6. Filling your code here */
+
+    /* 6. End of code */
+  }
+}
+ 
