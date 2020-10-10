308cf1c38208b834856a1b6fc8959c8ca61247c9
Md Ali <66300613+xXxSpicyBoiiixXx@users.noreply.github.com>
2020-10-10 15:30:41 -0700

    example image code

---
+++ Textbook-Notes/Chapter01/Classes-Objects-Applications/ImageGen01.java
@@ -0,0 +1,50 @@
+/*
+ * File: ImageGen01.java
+ * Author: xXxSpicyBoiiixXx
+ * Description: Image generation in java
+ * Date: 10/10/2020
+ */
+
+import java.awt.image.*;
+import java.awt.Color;
+import java.io.*;
+import javax.imageio.*;
+
+public class ImageGen01
+{
+	public static void main (String[] args) throws IOException 
+	{
+		// Destination file 
+		String fileOut = args[0];
+
+		// Creating Image of hardcoded size and type
+		final int SIDE = 1024;
+		final int TYPE = BufferedImage.TYPE_INT_RGB;
+		BufferedImage image = new BufferedImage(SIDE, SIDE, TYPE);
+		
+		// Limit of RGB values 
+		final int LIMIT = 255; 
+		
+		// Specific values of R G and B
+		int c;
+
+		Color color;
+
+	for(int i = 0; i < SIDE; i++)
+		for(int j = 0; j < SIDE; j++)
+		{
+			c = (i + j) % LIMIT;
+
+			// creates Gray values
+			color = new Color(c, c, c);
+
+			// Saving pixles
+			image.setRGB(i, j, color.getRGB()); 
+
+		}
+	
+	File outputfile = new File(fileOut);
+	ImageIO.write(image, "jpg", outputfile);
+	}
+}
+
