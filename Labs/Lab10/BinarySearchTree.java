7d64df1a2d90fd46c7e7121eb83aae7aa1a667aa
Md Ali <66300613+xXxSpicyBoiiixXx@users.noreply.github.com>
2020-11-24 06:34:30 -0600

    Lab 10 Complete

--- Labs/Lab10/BinarySearchTree.java
+++ Labs/Lab10/BinarySearchTree.java
@@ -27,7 +27,7 @@ public class BinarySearchTree {
         return root == null;
     }
 
- /////////////////// Your lab assignment starts from here //////////////////
+    /////////////////// Your lab assignment starts from here //////////////////
 
     /**
      * Returns true if this BST contains a node with integer c
@@ -47,8 +47,32 @@ public class BinarySearchTree {
      * @return
      */
     public boolean search(int c, BSTNode node) {
-        /* 1. Fill your code here */
+        /* 1. Fill your code here */;
+       if(node == null || c == node.getInfo())
+       {
+           if(node != null)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
 
+       else if(node.getInfo() < c)
+       {
+           return search(c, node.getRight());
+       }
+
+       else if(node.getInfo() > c)
+        {
+            return search(c, node.getLeft());
+        }
+
+       else{
+           return false;
+       }
         /* 1. End of your code */
     }
 
@@ -70,6 +94,20 @@ public class BinarySearchTree {
     public BSTNode recAdd(int c, BSTNode node) {
         /* 2. Fill your code here */
 
+        if(node == null)
+        {
+            node = new BSTNode(c);
+            return node;
+        }
+        if(c < node.getInfo())
+        {
+            node.setLeft(recAdd(c, node.getLeft()));
+        }
+        else if(c > node.getInfo())
+        {
+            node.setRight(recAdd(c, node.getRight()));
+        }
+        return node;
         /* 2. End of your code */
     }
 
@@ -79,7 +117,43 @@ public class BinarySearchTree {
      */
     public void iterAdd(int c) {
         /* 3. Fill your code here */
+        BSTNode newNode = new BSTNode(c);
+        BSTNode tempNode = root;
+        BSTNode swapNode = null;
+        if(tempNode == null)
+        {
+            add(c);
+        }
 
+        while(tempNode != null)
+        {
+            swapNode = tempNode;
+            if(c < tempNode.getInfo())
+            {
+                tempNode = tempNode.getLeft();
+            }
+            else
+            {
+                tempNode = tempNode.getRight();
+            }
+        }
+
+        if(swapNode == null)
+        {
+            add(c);
+        }
+
+        else if(c < swapNode.getInfo())
+        {
+            swapNode.setLeft(newNode);
+        }
+
+        else
+        {
+            swapNode.setRight(newNode);
+        }
+
+        add(c);
         /* 3. End of your code */
     }
 
@@ -91,7 +165,7 @@ public class BinarySearchTree {
         switch(t) {
             case PREORDER:
                 preorderDisplay(root);
-                break; 
+                break;
             case INORDER:
                 inorderDisplay(root);
                 break;
@@ -111,8 +185,12 @@ public class BinarySearchTree {
      */
     public void preorderDisplay(BSTNode node) {
         /* 4. Fill your code here */
-
-        /* 4. End of your code */
+        if (node != null) {
+            System.out.print(node.getInfo() + " ");
+            preorderDisplay(node.getLeft());
+            preorderDisplay(node.getRight());
+            /* 4. End of your code */
+        }
     }
 
     /**
@@ -123,9 +201,13 @@ public class BinarySearchTree {
      */
     public void inorderDisplay(BSTNode node) {
         /* 5. Fill your code here */
-
+        if(node != null){
+            inorderDisplay(node.getLeft());
+            System.out.print(node.getInfo() + " ");
+            inorderDisplay(node.getRight());
+        }
         /* 5. End of your code */
-    }
+   }
 
     /**
      * Prints the integers of the sbutree whose root is referenced by node.
@@ -135,7 +217,13 @@ public class BinarySearchTree {
      */
     public void postorderDisplay(BSTNode node) {
         /* 6. Fill your code here */
-
+        if(node != null)
+        {
+            postorderDisplay(node.getLeft());
+            postorderDisplay(node.getRight());
+            System.out.print(node.getInfo() + " ");
+        }
+        
         /* 6. End of your code */
     }
 
@@ -157,7 +245,21 @@ public class BinarySearchTree {
      */
     private int recMaxDepth(BSTNode node) {
         /* 7. Fill your code here */
+            if(node == null){
+                return 0;
+            }
+            else{
+                int leftDepth = recMaxDepth(node.getLeft());
+                int rightDepth = recMaxDepth(node.getRight());
 
+                if(leftDepth > rightDepth){
+                    return leftDepth+1;
+                }
+                else{
+                    return rightDepth+1;
+                }
+
+            }
         /* 7. End of your code */
     }
 }
--- Labs/Lab10/README.md
+++ Labs/Lab10/README.md
@@ -1,5 +1,13 @@
-# Lab 08
+# Lab 10
 
 ## Notes
 
-*
+* I did not do anything fancy and just kept to the lab instructions. I want to focus on the final project more so. 
+
+* To complie, please open your terminal and then move to the file directory, and run "make"
+
+* To run, please type in "make run" this will run the driver function provided, also known as Main.java file 
+
+* To clean, please type "make clean" 
+
+* If there are any questions or concerns, please let me know. 
